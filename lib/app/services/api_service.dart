61a4e19df70d35bc738c10e318c62ef75ce48aef
Mike Hickman <mike@pdxmike.com>
2020-03-14 21:51:34 -0700

    Create APIService post request for accessToken

---
+++ lib/app/services/api_service.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:coronavirus/app/services/api.dart';
+
+class APIService {
+  APIService(this.api);
+  final API api;
+
+  // add async before method because we're using await within it
+  Future<String> getAccessToken() async {
+    final response = await http.post(
+      api.tokenUri().toString(),
+      headers: {'Authorization': 'Basic ${api.apiKey}'},
+    );
+
+    if (response.statusCode == 200) {
+      // taking response body (string) and pass to json.decode, parse it and return a map of key/value pairs
+      // send request from ncov_2019_api.http to see
+      final data = json.decode(response.body);
+
+      // pull access_token key value from data map
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    print(
+      'Request ${api.tokenUri()} failed.\nResponse: ${response.statusCode} ${response.reasonPhrase}'
+    );
+    throw response;
+  }
+}
