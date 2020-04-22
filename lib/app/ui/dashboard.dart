0a54fafceb5ec7895392f85c2ed894feda7f0434
Mike Hickman <mike@pdxmike.com>
2020-04-22 06:21:46 -0700

    Add updateData method to show real endpoint data within endpoint card

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -1,6 +1,8 @@
+import 'package:coronavirus/app/repositories/data_repository.dart';
 import 'package:coronavirus/app/services/api.dart';
 import 'package:coronavirus/app/ui/endpoint_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -8,6 +10,24 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  int _cases;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+
+  Future<void> _updateData() async {
+    final dataRepository = Provider.of<DataRepository>(context, listen: false);
+    final cases = await dataRepository.getEndpointData(Endpoint.cases);
+    setState(
+      () {
+        _cases = cases;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +38,7 @@ class _DashboardState extends State<Dashboard> {
           children: <Widget>[
             EndpointCard(
               endpoint: Endpoint.cases,
-              value: 222,
+              value: _cases,
             ),
           ],
         ));
