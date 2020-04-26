3ebb6e06fc10de8c1d4338fa9e0ac552fc9553fe
Mike Hickman <mike@pdxmike.com>
2020-04-25 17:41:35 -0700

    Refactor to use EndpointData

--- lib/app/repositories/data_repository.dart
+++ lib/app/repositories/data_repository.dart
@@ -1,6 +1,7 @@
 import 'package:coronavirus/app/repositories/enpoints_data.dart';
 import 'package:coronavirus/app/services/api.dart';
 import 'package:coronavirus/app/services/api_service.dart';
+import 'package:coronavirus/app/services/endpoint_data.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 
@@ -10,8 +11,8 @@ class DataRepository {
 
   String _accessToken;
 
-  Future<int> getEndpointData(Endpoint endpoint) async =>
-    await _getDataRefreshingToken<int>(
+  Future<EndpointData> getEndpointData(Endpoint endpoint) async =>
+    await _getDataRefreshingToken<EndpointData>(
       onGetData: () => apiService.getEndpointData(
           accessToken: _accessToken, endpoint: endpoint),
     );
