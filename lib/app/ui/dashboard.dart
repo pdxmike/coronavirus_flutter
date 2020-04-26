922f964a1444b77cfba2b9cac3e2aceeee5e7ed9
Mike Hickman <mike@pdxmike.com>
2020-04-25 17:49:41 -0700

    Add last server update date/time to top of dash

--- lib/app/ui/dashboard.dart
+++ lib/app/ui/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:coronavirus/app/repositories/data_repository.dart';
 import 'package:coronavirus/app/repositories/enpoints_data.dart';
 import 'package:coronavirus/app/services/api.dart';
 import 'package:coronavirus/app/ui/endpoint_card.dart';
+import 'package:coronavirus/app/ui/last_updated_status_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -35,6 +36,12 @@ class _DashboardState extends State<Dashboard> {
           onRefresh: _updateData,
           child: ListView(
             children: <Widget>[
+              LastUpdatedStatusText(
+                text: _endpointsData != null
+                    ? _endpointsData.values[Endpoint.cases].date?.toString() ??
+                        ''
+                    : '',
+              ),
               for (var endpoint in Endpoint.values)
                 EndpointCard(
                   endpoint: endpoint,
