455c2dfed5c2511612a35cc312ee2b3068d50465
Mike Hickman <mike@pdxmike.com>
2020-04-25 19:46:32 -0700

    Add shared_preferences package and add DataCacheService

---
+++ lib/app/services/data_cache_service.dart
@@ -0,0 +1,44 @@
+import 'package:coronavirus/app/repositories/enpoints_data.dart';
+import 'package:coronavirus/app/services/api.dart';
+import 'package:coronavirus/app/services/endpoint_data.dart';
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DataCacheService {
+  DataCacheService({@required this.sharedPreferences});
+  final SharedPreferences sharedPreferences;
+
+  static String endpointValueKey(Endpoint endpoint) => '$endpoint/value';
+  static String endpointDateKey(Endpoint endpoint) => '$endpoint/date';
+
+  EndpointsData getData() {
+    Map<Endpoint, EndpointData> values = {};
+    Endpoint.values.forEach((endpoint) {
+      final value = sharedPreferences.getInt(endpointValueKey(endpoint));
+      final dateString = sharedPreferences.getString(endpointValueKey(endpoint));
+      if (value != null && dateString != null) {
+        final date = DateTime.tryParse(dateString);
+        values[endpoint] = EndpointData(
+          value: value,
+          date: date,
+        );
+      }
+    });
+    return EndpointsData(values: values);
+  }
+
+  Future<void> setData(EndpointsData endpointsData) async {
+    endpointsData.values.forEach(
+      (endpoint, endpointData) async {
+        await sharedPreferences.setInt(
+          endpointValueKey(endpoint),
+          endpointData.value,
+        );
+        await sharedPreferences.setString(
+          endpointDateKey(endpoint),
+          endpointData.date.toIso8601String(),
+        );
+      },
+    );
+  }
+}
